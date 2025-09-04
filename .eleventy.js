f85182d2f16f9d2dd4b0d6c0a1493969206ef489
ItsDoomOne <85998702+ItsDoomOne@users.noreply.github.com>
2025-09-04 20:49:33 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,14 @@
+const crypto = require("crypto");
+const fs = require("fs");
+
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addNunjucksFilter("hashFile", function(filename) {
+    const data = fs.readFileSync(`./_site/${filename}`);
+    const hash = crypto.createHash("md5").update(data).digest("hex").slice(0,8);
+    return `${filename}?v=${hash}`;
+  });
+
   return {
     pathPrefix: "/blog/",
   };
