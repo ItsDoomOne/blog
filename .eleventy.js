882b95504d0eab72383ce8eaeda002f14831bab2
ItsDoomOne <leodroid796@gmail.com>
2025-09-05 00:02:10 +0000

    dale gremio 5

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,8 @@
-const crypto = require("crypto");
-const fs = require("fs");
+const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
 
 module.exports = function(eleventyConfig) {
-
-  eleventyConfig.addNunjucksFilter("hashFile", function(filename) {
-    const data = fs.readFileSync(`./_site/${filename}`);
-    const hash = crypto.createHash("md5").update(data).digest("hex").slice(0,8);
-    return `${filename}?v=${hash}`;
-  });
-
+  const cacheBusterOptions = {};
+  eleventyConfig.addPlugin(cacheBuster, cacheBusterOptions);
   return {
     pathPrefix: "/blog/",
   };
