41cc8da29aeb404c00f341b1aef941c91a0fa12d
ItsDoomOne <85998702+ItsDoomOne@users.noreply.github.com>
2025-09-04 21:12:48 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
 const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
 
 module.exports = function(eleventyConfig) {
+  // ⚡ Plugin cache-buster
   const cacheBusterOptions = {};
   eleventyConfig.addPlugin(cacheBuster, cacheBusterOptions);
+
+  // ⚡ Ordena a collection "posts" do mais novo pro mais velho
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByTag("posts").sort((a, b) => b.date - a.date);
+  });
+
+  // ⚡ Configura pathPrefix pra GitHub Pages
   return {
     pathPrefix: "/blog/",
   };
